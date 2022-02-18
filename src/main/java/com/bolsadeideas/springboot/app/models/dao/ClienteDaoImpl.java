65e1cb5ddbf28c91b87c744818a10cd757d36144
Martín <magadiflo@gmail.com>
2022-02-17 22:51:43 -0500

    Creando la clase de Acceso a Datos o DAO con Entity Manager - @Repository

---
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+	//Al no haber configuración de persistencia, por defecto usará la BD H2 cuya dependencia está en el pom.xml
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		return this.em.createQuery("SELECT c FROM Cliente AS c", Cliente.class).getResultList();
+	}
+
+}
